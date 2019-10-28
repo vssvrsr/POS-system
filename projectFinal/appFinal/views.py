28d73368b56b2cdcf4ad8f3ebd34a8db9e8c6248
vssvrsr <vssvrsr@gmail.com>
2019-10-28 03:31:50 +0000

    added comment to appFinal views.py

--- projectFinal/appFinal/views.py
+++ projectFinal/appFinal/views.py
@@ -4,45 +4,60 @@ from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from .models import User, Customer, LogedIn, Employee, Shop
 
+
 def getIP(request):
     if 'HTTP_X_FORWARDED_FOR' in request.META:
-        ip =  request.META['HTTP_X_FORWARDED_FOR']
+        ip = request.META['HTTP_X_FORWARDED_FOR']
     else:
         ip = request.META['REMOTE_ADDR']
 
     try:
         user = LogedIn.objects.get(loged_ip=ip).loged_user
     except:
-        user=''
-    
-    tmp = {'ip':ip, 'user':user}
+        user = ''
+
+    tmp = {'ip': ip, 'user': user}
 
     return tmp
 
+
 def login(request):
-    if 'loginB' in request.POST:
+    """
+    歡迎畫面
+    ------------------------
+    讓使用者憑自己帳號登入
+    ------------------------
+    TODO: 帳密錯誤提示訊息
+    """
+
+    if 'loginB' in request.POST:  # 已提交了帳號密碼，開始進行確認
         userId = request.POST['userId']
         userPw = request.POST['userPw']
-        try:
+
+        try:  # 進資料庫比對帳號密碼
             User.objects.get(user_id=userId, user_pw=userPw)
             userNow = userId
 
             if 'HTTP_X_FORWARDED_FOR' in request.META:
-                ip =  request.META['HTTP_X_FORWARDED_FOR']
+                ip = request.META['HTTP_X_FORWARDED_FOR']
             else:
                 ip = request.META['REMOTE_ADDR']
 
             LogedIn.objects.get_or_create(loged_user=userId, loged_ip=ip)
 
             return redirect('/app/index')
-        except:
+
+        except:  # 比對帳號密碼失敗，轉跳/app
             return redirect('/app')
 
-    return render(request, 'login.html', locals())
+    else:  # 未提交帳號密碼，提供表單，等待提交
+        return render(request, 'login.html', locals())
+
 
 def logout(request, a):
     LogedIn.objects.filter(loged_user=a).delete()
     return redirect('/app')
+
 
 def index(request):
     userNow = getIP(request)
@@ -50,6 +65,7 @@ def index(request):
         return redirect('/app')
 
     return render(request, 'index.html', locals())
+
 
 def cus(request):
     userNow = getIP(request)
@@ -60,6 +76,7 @@ def cus(request):
     cusAll = Customer.objects.all().exclude(cus_seeable=False)
 
     return render(request, 'cus.html', locals())
+
 
 def addCus(request):
     userNow = getIP(request)
@@ -81,10 +98,12 @@ def addCus(request):
         cusAddr2 = request.POST['cusAddr2']
         cusEmail = request.POST['cusEmail']
         cusRemark = request.POST['cusRemark']
-        Customer.objects.create(cus_name=cusName, cus_name_en=cusEnName, cus_id=cusId, cus_class=cusClass, cus_idcard=cusIdCard, cus_sex=cusSex, cus_phone1=cusPhone1, cus_phone2=cusPhone2, cus_addr1=cusAddr1, cus_addr2=cusAddr2, cus_email=cusEmail, cus_bd=cusBd, cus_shop_id="tmp", cus_arr_date=cusArrDate, cus_source="tmp", cus_remark=cusRemark)
-    
+        Customer.objects.create(cus_name=cusName, cus_name_en=cusEnName, cus_id=cusId, cus_class=cusClass, cus_idcard=cusIdCard, cus_sex=cusSex, cus_phone1=cusPhone1, cus_phone2=cusPhone2,
+                                cus_addr1=cusAddr1, cus_addr2=cusAddr2, cus_email=cusEmail, cus_bd=cusBd, cus_shop_id="tmp", cus_arr_date=cusArrDate, cus_source="tmp", cus_remark=cusRemark)
+
         return redirect('/app/cus')
     return render(request, 'addCus.html', locals())
+
 
 def editCus(request, a):
     userNow = getIP(request)
@@ -136,16 +155,18 @@ def editCus(request, a):
         cusAddr2 = request.POST['cusAddr2']
         cusEmail = request.POST['cusEmail']
         cusRemark = request.POST['cusRemark']
-        Customer.objects.filter(cus_id=a).update(cus_name=cusName, cus_name_en=cusEnName, cus_id=cusId, cus_class=cusClass, cus_idcard=cusIdCard, cus_sex=cusSex, cus_phone1=cusPhone1, cus_phone2=cusPhone2, cus_addr1=cusAddr1, cus_addr2=cusAddr2, cus_email=cusEmail, cus_bd=cusBd, cus_shop_id="tmp", cus_arr_date=cusArrDate, cus_source="tmp", cus_remark=cusRemark)
-        
+        Customer.objects.filter(cus_id=a).update(cus_name=cusName, cus_name_en=cusEnName, cus_id=cusId, cus_class=cusClass, cus_idcard=cusIdCard, cus_sex=cusSex, cus_phone1=cusPhone1,
+                                                 cus_phone2=cusPhone2, cus_addr1=cusAddr1, cus_addr2=cusAddr2, cus_email=cusEmail, cus_bd=cusBd, cus_shop_id="tmp", cus_arr_date=cusArrDate, cus_source="tmp", cus_remark=cusRemark)
+
         return redirect('/app/cus')
 
     if 'removeB' in request.POST:
         Customer.objects.filter(cus_id=a).update(cus_seeable=False)
 
-        return redirect('/app/cus')       
+        return redirect('/app/cus')
 
     return render(request, 'editCus.html', locals())
+
 
 def removed(request, a):
     persMenuOpen = "active menu-open"
@@ -166,23 +187,26 @@ def removed(request, a):
             removed.id = removed.cus_id
     return render(request, 'removed.html', locals())
 
+
 def restore(request, a, b):
     if a == 'emp':
         Employee.objects.filter(emp_id=b).update(emp_seeable=True)
-        return redirect('/app/removed/emp') 
+        return redirect('/app/removed/emp')
 
     if a == 'cus':
         Customer.objects.filter(cus_id=b).update(cus_seeable=True)
-        return redirect('/app/removed/cus')    
+        return redirect('/app/removed/cus')
+
 
 def delete(request, a, b):
     if a == 'emp':
         Employee.objects.get(emp_id=b).delete()
-        return redirect('/app/removed/emp')   
+        return redirect('/app/removed/emp')
 
     if a == 'cus':
         Customer.objects.get(cus_id=b).delete()
-        return redirect('/app/removed/cus')    
+        return redirect('/app/removed/cus')
+
 
 def emp(request):
     userNow = getIP(request)
@@ -193,6 +217,7 @@ def emp(request):
     empAll = Employee.objects.all().exclude(emp_seeable=False)
 
     return render(request, 'emp.html', locals())
+
 
 def addEmp(request):
     userNow = getIP(request)
@@ -216,10 +241,12 @@ def addEmp(request):
         empAddr2 = request.POST['empAddr2']
         empEmail = request.POST['empEmail']
         empRemark = request.POST['empRemark']
-        Employee.objects.create(emp_name_ch=empName, emp_name_en=empEnName, emp_id=empId, emp_class=empClass, emp_idcard=empIdCard, emp_salary=empSalary, emp_phone1=empPhone1, emp_phone2=empPhone2, emp_addr1=empAddr1, emp_addr2=empAddr2, emp_email=empEmail, emp_bd=empBd, emp_shop_id="tmp", emp_arr_date=empArrDate, emp_leave_date=empLeaveDate, emp_remark=empRemark)
-    
+        Employee.objects.create(emp_name_ch=empName, emp_name_en=empEnName, emp_id=empId, emp_class=empClass, emp_idcard=empIdCard, emp_salary=empSalary, emp_phone1=empPhone1, emp_phone2=empPhone2,
+                                emp_addr1=empAddr1, emp_addr2=empAddr2, emp_email=empEmail, emp_bd=empBd, emp_shop_id="tmp", emp_arr_date=empArrDate, emp_leave_date=empLeaveDate, emp_remark=empRemark)
+
         return redirect('/app/emp')
     return render(request, 'addEmp.html', locals())
+
 
 def editEmp(request, a):
     userNow = getIP(request)
@@ -228,7 +255,7 @@ def editEmp(request, a):
     persMenuOpen = "active menu-open"
 
     empThis = Employee.objects.get(emp_id=a)
- 
+
     empThisClassOther = ['頭皮護理師', '資深設計師', '設計師', '助理']
     if empThis.emp_class == '頭皮護理師':
         empThisClass = '頭皮護理師'
@@ -261,8 +288,9 @@ def editEmp(request, a):
         empAddr2 = request.POST['empAddr2']
         empEmail = request.POST['empEmail']
         empRemark = request.POST['empRemark']
-        Employee.objects.filter(emp_id=a).update(emp_name_ch=empName, emp_name_en=empEnName, emp_id=empId, emp_class=empClass, emp_idcard=empIdCard, emp_salary=empSalary, emp_phone1=empPhone1, emp_phone2=empPhone2, emp_addr1=empAddr1, emp_addr2=empAddr2, emp_email=empEmail, emp_bd=empBd, emp_shop_id="tmp", emp_arr_date=empArrDate, emp_leave_date=empLeaveDate, emp_remark=empRemark)
-    
+        Employee.objects.filter(emp_id=a).update(emp_name_ch=empName, emp_name_en=empEnName, emp_id=empId, emp_class=empClass, emp_idcard=empIdCard, emp_salary=empSalary, emp_phone1=empPhone1,
+                                                 emp_phone2=empPhone2, emp_addr1=empAddr1, emp_addr2=empAddr2, emp_email=empEmail, emp_bd=empBd, emp_shop_id="tmp", emp_arr_date=empArrDate, emp_leave_date=empLeaveDate, emp_remark=empRemark)
+
         return redirect('/app/emp')
 
     if 'removeB' in request.POST:
@@ -271,12 +299,14 @@ def editEmp(request, a):
 
     return render(request, 'editEmp.html', locals())
 
+
 def stock(request):
     userNow = getIP(request)
     if not LogedIn.objects.filter(loged_ip=userNow['ip']):
         return redirect('/app')
     stockMenuOpen = "active menu-open"
     return render(request, 'stock.html', locals())
+
 
 def sale(request):
     userNow = getIP(request)
@@ -285,12 +315,14 @@ def sale(request):
     saleMenuOpen = "active menu-open"
     return render(request, 'sale.html', locals())
 
+
 def service(request):
     userNow = getIP(request)
     if not LogedIn.objects.filter(loged_ip=userNow['ip']):
         return redirect('/app')
     saleMenuOpen = "active menu-open"
     return render(request, 'service.html', locals())
+
 
 def addStock(request):
     userNow = getIP(request)
@@ -299,12 +331,14 @@ def addStock(request):
     stockMenuOpen = "active menu-open"
     return render(request, 'addStock.html', locals())
 
+
 def deduct(request):
     userNow = getIP(request)
     if not LogedIn.objects.filter(loged_ip=userNow['ip']):
         return redirect('/app')
     saleMenuOpen = "active menu-open"
     return render(request, 'deduct.html', locals())
+
 
 def exportStock(request):
     userNow = getIP(request)
@@ -313,12 +347,14 @@ def exportStock(request):
     stockMenuOpen = "active menu-open"
     return render(request, 'exportStock.html', locals())
 
+
 def importStock(request):
     userNow = getIP(request)
     if not LogedIn.objects.filter(loged_ip=userNow['ip']):
         return redirect('/app')
     stockMenuOpen = "active menu-open"
     return render(request, 'importStock.html', locals())
+
 
 def report(request):
     userNow = getIP(request)
@@ -327,12 +363,14 @@ def report(request):
     reportMenuOpen = "active menu-open"
     return render(request, 'report.html', locals())
 
+
 def salaryCount(request):
     userNow = getIP(request)
     if not LogedIn.objects.filter(loged_ip=userNow['ip']):
         return redirect('/app')
     reportMenuOpen = "active menu-open"
     return render(request, 'salaryCount.html', locals())
+
 
 def turnoverCount(request):
     userNow = getIP(request)
@@ -341,23 +379,26 @@ def turnoverCount(request):
     reportMenuOpen = "active menu-open"
     return render(request, 'turnoverCount.html', locals())
 
+
 def setting(request):
     userNow = getIP(request)
     if not LogedIn.objects.filter(loged_ip=userNow['ip']):
         return redirect('/app')
     return render(request, 'setting.html', locals())
 
+
 def setShop(request):
     userNow = getIP(request)
     if not LogedIn.objects.filter(loged_ip=userNow['ip']):
-        return redirect('/app')   
-    
+        return redirect('/app')
+
     return render(request, 'setShop.html', locals())
+
 
 def addShop(request):
     userNow = getIP(request)
     if not LogedIn.objects.filter(loged_ip=userNow['ip']):
-        return redirect('/app')  
+        return redirect('/app')
 
     if 'saveB' in request.POST:
         shop_name = request.POST['shop_name']
@@ -368,8 +409,9 @@ def addShop(request):
         shop_phone2 = request.POST['shop_phone2']
         shop_remark = request.POST['shop_remark']
 
-        Shop.objects.create(shop_name=shop_name, shop_id=shop_id, shop_class=shop_class, shop_addr=shop_addr, shop_phone1=shop_phone1, shop_phone2=shop_phone2, shop_remark=shop_remark)
+        Shop.objects.create(shop_name=shop_name, shop_id=shop_id, shop_class=shop_class, shop_addr=shop_addr,
+                            shop_phone1=shop_phone1, shop_phone2=shop_phone2, shop_remark=shop_remark)
 
         return redirect('/app/setting/shop')
-    
+
     return render(request, 'addShop.html', locals())
